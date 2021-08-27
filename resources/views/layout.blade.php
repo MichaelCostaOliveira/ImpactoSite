a32ecf9082c82a4d1ee6b281121240a8005d26ff
Michael Costa <37298354+MichaelCostaOliveira@users.noreply.github.com>
2021-08-27 10:30:20 -0300

    ajuste variavel env

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,8 +9,8 @@
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="{{ asset('img/Logo_site.png') }}" rel="icon">
-    <link href="{{ asset('img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+    <link href="{{ asset('img/Logo_site.png', env('SECURE_PATH',  null)) }}" rel="icon">
+    <link href="{{ asset('img/apple-touch-icon.png', env('SECURE_PATH',  null)) }}" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
     <link
@@ -18,18 +18,18 @@
         rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="{{ asset('vendor/bootstrap/css/bootstrap.css') }}" rel="stylesheet"/>
-    <link href="{{ asset('vendor/icofont/icofont.min.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('vendor/bootstrap/css/bootstrap.css', env('SECURE_PATH',  null)) }}" rel="stylesheet"/>
+    <link href="{{ asset('vendor/icofont/icofont.min.css', env('SECURE_PATH',  null)) }}" rel="stylesheet"/>
 
-    <link href="{{ asset('vendor/boxicons/css/boxicons.css') }}" rel="stylesheet"/>
-    <link href="{{ asset('vendor/venobox/venobox.css') }}" rel="stylesheet"/>
-    <link href="{{ asset('vendor/owl.carousel/assets/owl.carousel.css') }}" rel="stylesheet"/>
-    <link href="{{ asset('vendor/aos/aos.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('vendor/boxicons/css/boxicons.css', env('SECURE_PATH',  null)) }}" rel="stylesheet"/>
+    <link href="{{ asset('vendor/venobox/venobox.css', env('SECURE_PATH',  null)) }}" rel="stylesheet"/>
+    <link href="{{ asset('vendor/owl.carousel/assets/owl.carousel.css', env('SECURE_PATH',  null)) }}" rel="stylesheet"/>
+    <link href="{{ asset('vendor/aos/aos.css', env('SECURE_PATH',  null)) }}" rel="stylesheet"/>
     <link
         href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap"
         rel="stylesheet">
     <!-- Template Main CSS File -->
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('css/style.css', env('SECURE_PATH',  null)) }}" rel="stylesheet"/>
 
     <!-- =======================================================
     * Template Name: Squadfree - v2.3.1
@@ -122,17 +122,17 @@
     </div>
 </footer>
 <!-- Vendor JS Files -->
-<script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-<script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.js') }}"></script>
-<script src="{{ asset('vendor/jquery.easing/jquery.easing.min.js') }}"></script>
-<script src="{{ asset('vendor/waypoints/jquery.waypoints.min.js') }}"></script>
-<script src="{{ asset('vendor/counterup/counterup.min.js') }}"></script>
-<script src="{{ asset('vendor/isotope-layout/isotope.pkgd.js') }}"></script>
-<script src="{{ asset('vendor/venobox/venobox.js') }}"></script>
-<script src="{{ asset('vendor/owl.carousel/owl.carousel.js') }}"></script>
-<script src="{{ asset('vendor/aos/aos.js') }}"></script>
+<script src="{{ asset('vendor/jquery/jquery.min.js', env('SECURE_PATH',  null)) }}"></script>
+<script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.js', env('SECURE_PATH',  null)) }}"></script>
+<script src="{{ asset('vendor/jquery.easing/jquery.easing.min.js', env('SECURE_PATH',  null)) }}"></script>
+<script src="{{ asset('vendor/waypoints/jquery.waypoints.min.js', env('SECURE_PATH',  null)) }}"></script>
+<script src="{{ asset('vendor/counterup/counterup.min.js', env('SECURE_PATH',  null)) }}"></script>
+<script src="{{ asset('vendor/isotope-layout/isotope.pkgd.js', env('SECURE_PATH',  null)) }}"></script>
+<script src="{{ asset('vendor/venobox/venobox.js', env('SECURE_PATH',  null)) }}"></script>
+<script src="{{ asset('vendor/owl.carousel/owl.carousel.js', env('SECURE_PATH',  null)) }}"></script>
+<script src="{{ asset('vendor/aos/aos.js', env('SECURE_PATH',  null)) }}"></script>
 <!-- Template Main JS File -->
-<script src="{{ asset('js/main.js') }}"></script>
+<script src="{{ asset('js/main.js', env('SECURE_PATH',  null)) }}"></script>
 <script>
 @if(Route::getCurrentRoute()->getName() == 'home')
 $('.menu-home').click(function() {
