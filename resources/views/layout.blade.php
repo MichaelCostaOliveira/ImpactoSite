fdd3b70ea1e7f0c51338dc112861830f61052d15
Michael Costa <37298354+MichaelCostaOliveira@users.noreply.github.com>
2021-09-27 08:47:56 -0300

    teste arquivos com variavel ambiente

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Academia Impacto</title>
+    <title>Vem Com a Impacto</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
